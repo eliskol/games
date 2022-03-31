73698a6aa1fd5f4b23726a6da4499a5560266d1a
Elias <75465281+eliskol@users.noreply.github.com>
2022-03-31 20:09:03 +0000

    separate random player into different file

---
+++ games/tic-tac-toe/random_player.py
@@ -0,0 +1,10 @@
+import random
+
+
+class RandomPlayer:
+    def choose_move(self, board):
+        move = random.randrange(0, 9)
+        while board[move] != 0:
+            move = random.randrange(0, 9)
+        return move
+    
