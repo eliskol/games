9a9794a65554c30ff30a647b805c95c65c82407a
Elias <75465281+eliskol@users.noreply.github.com>
2022-03-31 20:10:03 +0000

    just testing once, with logging on

--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -2,15 +2,15 @@ import sys
 sys.path.insert(1, sys.path[0].replace('tests', 'games/tic-tac-toe'))
 
 
-from random_tic_tac_toe_player import RandomPlayer
+from random_player import RandomPlayer
 from tic_tac_toe import Game
 
 player1 = RandomPlayer()
 player2 = RandomPlayer()
 outcomes = {'Tie': 0, 1: 0, 2: 0}
-for i in range(10000):
+for i in range(1):
     game = Game(player2, player1)
-    game.run(log=False)
+    game.run(log=True)
     # print(game.winner)
     outcomes[game.winner] += 1
     if i % 1000 == 0:
