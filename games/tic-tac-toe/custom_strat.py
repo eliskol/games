f3b5b30721600b5cd836ead28b08f4488cd56f3f
Elias <75465281+eliskol@users.noreply.github.com>
2022-04-13 22:02:27 +0000

    new strat? idk

--- games/tic-tac-toe/custom_strat.py
+++ games/tic-tac-toe/custom_strat.py
@@ -6,6 +6,12 @@ from tic_tac_toe import Game
 
 def strategy(board):
 
+    player_number = 2
+
+    if board == [0 for _ in range(9)]:
+        player_number = 1
+    
+
     for j in range(3):
         i = 3 * j
         if board[j] == board[j + 3] != 0 and board[j + 6] == 0:  # columns
@@ -34,22 +40,31 @@ def strategy(board):
     elif board[4] == board[6] != 0 and board[2] == 0:
         return 2
 
-    if board == [0 for _ in range(9)]:
-        return 4
-    elif board[0] == 0:
-        return 0
-    elif board[2] == 0:
-        return 2
-    elif board[1] == 0:
-        return 1
-    elif board[8] == 0:
-        return 8
-    elif board[5] == 0:
-        return 5
-    elif board[6] == 0:
-        return 6
-    elif board[4] == 0:
-        return 4
+    if player_number == 1:
+
+        if board == [0 for _ in range(9)]:
+            return 4
+
+        elif board[1] == 0:
+            return 1
+        elif board[3] == 0:
+            return 3
+        elif board[5] == 0:
+            return 5
+        elif board[7] == 0:
+            return 7
+
+    if player_number == 2:
+        if board[0] == 0:
+            return 0
+        elif board[2] == 0:
+            return 2
+        elif board[6] == 0:
+            return 6
+        elif board[8] == 0:
+            return 8
+
+
 
     random_move = random.randrange(0, 9)
     while board[random_move] != 0:
