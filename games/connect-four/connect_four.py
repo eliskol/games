fcb4f7000de4d7e785de0270830ae7986e599c1e
Elias <75465281+eliskol@users.noreply.github.com>
2022-05-12 23:14:57 +0000

    fixed c4 win detection, completed c4 game class + tests

--- games/connect-four/connect_four.py
+++ games/connect-four/connect_four.py
@@ -1,17 +1,21 @@
 class Game:
     def __init__(self, player1, player2):
-        self.player1 = player1
-        self.player2 = player2
+        # self.player1 = player1
+        # self.player2 = player2
         self.players = [player1, player2]
         self.next_player = 1
         self.moves = 0
         self.board = [[0 for _ in range(7)] for _ in range(6)]
 
+
     def check_move_validity(self, move):
-        skip
+        for row in self.board:
+            if row[move] == 0:
+                return True
+        return False
 
     def print_board(self):
-        for row in self.board:
+        for row in reversed(self.board):
             print(row)
 
     def check_move_validity(self, move):
@@ -21,17 +25,44 @@ class Game:
         return False
 
     def determine_winner(self):
-        player_that_made_move = self.next_player = [2, 1][self.next_player - 1]
-        previous_column_move = self.previous_move
-        for row in reversed(range(6)):
-            if self.board[row][column] == player_that_made_move:
-                row_that_token_dropped_to = row
+        if self.board == [[0 for _ in range(7)] for _ in range(6)]:
+            return None
+
+        player_that_made_move = self.previous_player
+
+        for i in range(0, 6):
+            for j in range(0, 2):
+                if self.board[i][j] == self.board[i][j + 1] == self.board[i][j + 2] == self.board[i][j + 3] == player_that_made_move:
+                    return player_that_made_move
+
+
+        for i in range(0, 3):
+            for j in range(0, 5):
+                if self.board[i][j] == self.board[i + 1][j] == self.board[i + 2][j] == self.board[i + 3][j] == player_that_made_move:
+                    return player_that_made_move
+
+        for i in range(0, 3):
+            for j in range(0, 2):
+                if self.board[i][j] == self.board[i + 1][j + 1] == self.board[i + 2][j + 2] == self.board[i + 3][j + 3] == player_that_made_move:
+                    return player_that_made_move
+
+                elif self.board[5 - i][j] == self.board[5 - (i + 1)][j + 1] == self.board[5 - (i + 2)][j + 2] == self.board[5 - (i + 3)][j + 3] == player_that_made_move:
+                    return player_that_made_move
+
         
+        if any(0 in row for row in self.board):
+            pass
+        else:
+            return 'Tie'
+        
+        return None
+
     
     def drop_token(self, player, column):
         for row in range(6):
             if self.board[row][column] == 0:
                 self.board[row][column] = player
+                break
 
     def make_move(self):
         self.moves += 1
@@ -40,7 +71,7 @@ class Game:
         validity_of_move = self.check_move_validity(move_to_make)
         if validity_of_move is True:
             self.drop_token(self.next_player, move_to_make)
-            self.previous_move = move_to_make
+            self.previous_player = self.next_player
         if self.log is True:
             print('----------')
             self.print_board()
@@ -55,9 +86,28 @@ class Game:
         self.log = log
         self.winner = self.determine_winner()
         while self.winner is None:
+            # input()
+            # print(self.next_player)
             self.make_move()
             self.winner = self.determine_winner()
 
 
-bruh = Game('a', 'a')
-bruh.print_board()
+
+from random_player import RandomPlayer
+
+
+class InputPlayer:
+    def choose_move(self, board):
+        for row in reversed(board):
+            print(row)
+        print("Which column would you like to drop down?")
+        move = input()
+        return int(move)
+
+a = InputPlayer()
+b = InputPlayer()
+
+bruh = Game(a, b)
+bruh.run(log=True)
+print('winner: ', bruh.winner)
+# bruh.print_board()
