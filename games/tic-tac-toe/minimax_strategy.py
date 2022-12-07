b067e61510eaee5c341be9a851aa5f37463b9660
Elias <75465281+eliskol@users.noreply.github.com>
2022-12-07 23:39:19 +0000

    minimax strategy in progres

---
+++ games/tic-tac-toe/minimax_strategy.py
@@ -0,0 +1,12 @@
+from tic_tac_toe_recombining_tree import TicTacToeRecombiningTree
+
+
+class MinimaxStrategy:
+    def __init__(self):
+        self.generate_tree()
+
+    def generate_tree(self):
+        self.tree = TicTacToeRecombiningTree()
+
+    def propagate_minimax_values(self):
+        pass
