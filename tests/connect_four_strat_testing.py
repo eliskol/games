00a9b7d24aca828b06d161ff313eefa738393338
Elias <75465281+eliskol@users.noreply.github.com>
2023-03-23 02:17:10 +0000

    lots of testing

--- tests/connect_four_strat_testing.py
+++ tests/connect_four_strat_testing.py
@@ -1,4 +1,4 @@
-from random import Random, random
+import random
 import sys
 sys.path.insert(1, sys.path[0].replace('tests', 'games/connect-four'))
 
@@ -7,19 +7,29 @@ from random_player import RandomPlayer
 from custom_strat import CustomStrat
 from christine import CustomPlayer
 from heuristic_minimax_strategy import HeuristicMinimaxStrategy
+from last_minute_player import LastMinutePlayer
+from input_player import InputPlayer
 
+toLog = False
 random_player = RandomPlayer()
-custom_player = HeuristicMinimaxStrategy(5)
+random_heuristic_player = HeuristicMinimaxStrategy(4, True)
+last_minute_player = LastMinutePlayer()
+custom_player = HeuristicMinimaxStrategy(3, False)
+custom_player2 = HeuristicMinimaxStrategy(2, False)
+input_player = InputPlayer()
+
+
 outcomes = {'Tie': 0, 'c': 0, 'r': 0}
-for i in range(1):
+for i in range(100):
     if i % 2 == 0:
-        game = Game(custom_player, random_player)
-        game.run()
+        game = Game(custom_player, custom_player2)
+        game.run(log=toLog)
         player_order = {'Tie': 'Tie', 1: 'c', 2: 'r'}
     elif i % 2 == 1:
-        game = Game(random_player, custom_player)
-        game.run()
+        game = Game(custom_player2, custom_player)
+        game.run(log=toLog)
         player_order = {'Tie': 'Tie', 1: 'r', 2: 'c'}
     outcomes[player_order[game.winner]] += 1
+    print('game number', i + 1, 'finished')
 print(outcomes)
 # comment phase
