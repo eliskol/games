19034325edb56820fa3b838349f5d4082397f3f4
Elias <75465281+eliskol@users.noreply.github.com>
2023-03-23 02:16:28 +0000

    go in the next available column

--- games/connect-four/custom_strat.py
+++ games/connect-four/custom_strat.py
@@ -1,3 +1,6 @@
 class CustomStrat:
     def choose_move(self, board):
-        return 3
+        move = 3
+        while 0 not in [board[i][move] for i in range(6)]:
+            move = (move + 1) % 7
+        return move
