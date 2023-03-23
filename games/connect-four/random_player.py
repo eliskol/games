c022faab92020d0729b72a91fc002b6b54dc3ccc
Elias <75465281+eliskol@users.noreply.github.com>
2023-03-23 02:16:56 +0000

    forced random player to make valid moves

--- games/connect-four/random_player.py
+++ games/connect-four/random_player.py
@@ -4,4 +4,6 @@ import random
 class RandomPlayer:
     def choose_move(self, board):
         move = random.randrange(0, 7)
+        while 0 not in [board[i][move] for i in range(6)]:
+            move = random.randrange(0, 7)
         return move
