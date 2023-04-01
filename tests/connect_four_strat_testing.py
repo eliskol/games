d11cfa9b3b03439716a49c9cc0ae02ed8708c612
Elias <75465281+eliskol@users.noreply.github.com>
2023-04-01 06:17:16 +0000

    more testing: 4-ply minimax beats 5-ply everytime...

--- tests/connect_four_strat_testing.py
+++ tests/connect_four_strat_testing.py
@@ -1,29 +1,35 @@
-import random
 import sys
 sys.path.insert(1, sys.path[0].replace('tests', 'games/connect-four'))
-
-from connect_four import Game
-from random_player import RandomPlayer
-from custom_strat import CustomStrat
-from christine import CustomPlayer
-from heuristic_minimax_strategy import HeuristicMinimaxStrategy
-from last_minute_player import LastMinutePlayer
+sys.path.insert(1, sys.path[0].replace('tests', 'games/connect-four/celeste-strat'))
+from random_heuristic_strategy import RandomHeuristicStrategy
 from input_player import InputPlayer
+from last_minute_player import LastMinutePlayer
+from heuristic_minimax_strategy import HeuristicMinimaxStrategy
+from christine import CustomPlayer
+from custom_strat import CustomStrat
+from random_player import RandomPlayer
+from connect_four import Game
+from heuristic_player import *
+import random
 
-toLog = True
+end_game = None
+
+toLog = False
 
 first_player = HeuristicMinimaxStrategy(5)
-second_player = HeuristicMinimaxStrategy(2)
+second_player = HeuristicMinimaxStrategy(4)
 
 outcomes = {'Tie': 0, 'c': 0, 'r': 0}
-for i in range(1):
+for i in range(10):
     if i % 2 == 0:
         game = Game(first_player, second_player)
         game.run(log=toLog)
+        end_game = game.board
         player_order = {'Tie': 'Tie', 1: 'c', 2: 'r'}
     elif i % 2 == 1:
         game = Game(second_player, first_player)
         game.run(log=toLog)
+        # assert game.board == end_game
         player_order = {'Tie': 'Tie', 1: 'r', 2: 'c'}
     outcomes[player_order[game.winner]] += 1
     print('game number', i + 1, 'finished')
