ab6eaa61eead005edf9190ca263b0e67b2801e48
Elias <75465281+eliskol@users.noreply.github.com>
2023-04-08 05:44:14 +0000

    did profiling with cProfile, seems like caching the calculated heuristic values could save some time

--- tests/pickle_testing.py
+++ tests/pickle_testing.py
@@ -4,7 +4,7 @@ sys.path.insert(1, sys.path[0].replace('tests', 'games/connect-four'))
 from heuristic_minimax_strategy import HeuristicMinimaxStrategy
 from connect_four_recombining_tree_custom_depth import ConnectFourRecombiningTreeCustomDepth
 
-tree = ConnectFourRecombiningTreeCustomDepth([[0 for _ in range(7)] for _ in range(6)], 4)
+tree = ConnectFourRecombiningTreeCustomDepth([[0 for _ in range(7)] for _ in range(6)], 4)  # RecursionError with ply > 3
 
 with open('data.pickle', 'wb') as f:
     pickle.dump(tree.node_dict, f, pickle.HIGHEST_PROTOCOL)
