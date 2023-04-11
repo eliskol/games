d565b89cc9cc2b816e2586b51aae349744fcd4fe
Elias <75465281+eliskol@users.noreply.github.com>
2023-04-11 22:41:46 +0000

    me vs Jeff

--- tests/connect_four_strat_testing.py
+++ tests/connect_four_strat_testing.py
@@ -4,38 +4,35 @@ sys.path.insert(1, sys.path[0].replace(
     'tests', 'games/connect-four/celeste-strat'))
 sys.path.insert(1, sys.path[0].replace(
     'tests', 'games/connect-four/jeff-strat'))
-from random_heuristic_strategy import RandomHeuristicStrategy
-from input_player import InputPlayer
-from last_minute_player import LastMinutePlayer
-from heuristic_minimax_strategy import HeuristicMinimaxStrategy
-from christine import CustomPlayer
-from custom_strat import CustomStrat
-from random_player import RandomPlayer
-from connect_four import Game
-from predefined_strat import PredefinedPlayer
-from c_four_minimax import minimaxHeuristic
 import random
+from c_four_minimax import minimaxHeuristic
+from predefined_strat import PredefinedPlayer
+from connect_four import Game
+from random_player import RandomPlayer
+from custom_strat import CustomStrat
+from christine import CustomPlayer
+from heuristic_minimax_strategy import HeuristicMinimaxStrategy
+from last_minute_player import LastMinutePlayer
+from input_player import InputPlayer
+from random_heuristic_strategy import RandomHeuristicStrategy
 
 end_game = None
 
 toLog = True
 
-first_player = HeuristicMinimaxStrategy(5)
-# second_player = HeuristicMinimaxStrategy(5)
-
-# trying to figure out why predefined player wins here
-# thinking its bc minimax values are off
+first_player = HeuristicMinimaxStrategy(5, True)
+# second_player = RandomPlayer()
 
 outcomes = {'Tie': 0, 'c': 0, 'r': 0}
 for i in range(2):
     if i % 2 == 0:
-        second_player = minimaxHeuristic(2, 3)
+        second_player = minimaxHeuristic(2, 5)
         game = Game(first_player, second_player)
         game.run(log=toLog)
         end_game = game.board
         player_order = {'Tie': 'Tie', 1: 'c', 2: 'r'}
     elif i % 2 == 1:
-        second_player = minimaxHeuristic(1, 3)
+        second_player = minimaxHeuristic(1, 5)
         game = Game(second_player, first_player)
         game.run(log=toLog)
         # assert game.board == end_game
